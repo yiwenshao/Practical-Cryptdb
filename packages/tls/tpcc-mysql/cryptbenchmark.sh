7e6eccb6b2e6992ad2e09f1ddc9045c7587361e3
casualet <shaoyiwenet@gmail.com>
2017-05-11 20:37:44 -0400

    add bench

--- packages/tls/tpcc-mysql/cryptbenchmark.sh
+++ packages/tls/tpcc-mysql/cryptbenchmark.sh
@@ -1,4 +1,8 @@
-mysqladmin create tpcc1000 -uroot -pletmein -h127.0.0.1 -P3399
+echo "=============================================CREATE DATABASE====================================================="
+mysql -uroot -pletmein -h127.0.0.1 -P3399 -e "create database tpcc1000"
+echo "=============================================CREATE TABLE====================================================="
 mysql -uroot -pletmein -h127.0.0.1 -P3399 tpcc1000 < create_table.sql
+echo "=============================================LOAD DATA====================================================="
 mysql -uroot -pletmein -h127.0.0.1 -P3399 tpcc1000 < all.sql
-mysqldump --skip-extended-insert -uroot -pletmein -h127.0.0.1 -P3399 tpcc1000 > crypt.sql
+echo "=============================================DUMP DATABASE====================================================="
+mysqldump --skip-extended-insert -uroot -pletmein -h127.0.0.1 -P3399 tpcc1000 > back.sql
--- packages/tls/tpcc-mysql/normalbenchmark.sh
+++ packages/tls/tpcc-mysql/normalbenchmark.sh
@@ -1,4 +1,8 @@
-mysqladmin create tpcc1000 -uroot -pletmein -h127.0.0.1
+
+# load data and dump all.sql
+mysql -uroot -pletmein -h127.0.0.1 -e "drop database if exists tpcc1000"
+mysql -uroot -pletmein -h127.0.0.1 -e "create database tpcc1000"
 mysql -uroot -pletmein -h127.0.0.1 tpcc1000 < create_table.sql
 ./tpcc_load -h127.0.0.1 -uroot -pletmein -d tpcc1000 -w 2
 mysqldump --skip-extended-insert -uroot -pletmein -h127.0.0.1 tpcc1000 > all.sql
+mysql -uroot -pletmein -h127.0.0.1 -e "drop database if exists tpcc1000"
