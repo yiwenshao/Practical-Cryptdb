871756bd12941f3b0a7676b4d8e3654340a7df1c
casualet <shaoyiwenet@gmail.com>
2017-05-11 21:24:19 -0400

    add benchmark

--- packages/tls/tpcc-mysql/cryptbenchmark.sh
+++ packages/tls/tpcc-mysql/cryptbenchmark.sh
@@ -5,4 +5,4 @@ mysql -uroot -pletmein -h127.0.0.1 -P3399 tpcc1000 < create_table.sql
 echo "=============================================LOAD DATA====================================================="
 mysql -uroot -pletmein -h127.0.0.1 -P3399 tpcc1000 < all.sql
 echo "=============================================DUMP DATABASE====================================================="
-mysqldump --skip-extended-insert -uroot -pletmein -h127.0.0.1 -P3399 tpcc1000 > back.sql
+mysqldump --skip-extended-insert -uroot -pletmein -h127.0.0.1 tpcc1000 > back.sql
