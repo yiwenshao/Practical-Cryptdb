b3df5e91c699316a5e00257a4999f977866d19b1
casualet <shaoyiwenet@gmail.com>
2017-05-17 06:56:09 -0400

    able to specify num of warehouse

--- packages/tls/tpcc-mysql/normalbenchmark.sh
+++ packages/tls/tpcc-mysql/normalbenchmark.sh
@@ -3,6 +3,6 @@
 mysql -uroot -pletmein -h127.0.0.1 -e "drop database if exists tpcc1000"
 mysql -uroot -pletmein -h127.0.0.1 -e "create database tpcc1000"
 mysql -uroot -pletmein -h127.0.0.1 tpcc1000 < create_table.sql
-./tpcc_load -h127.0.0.1 -uroot -pletmein -d tpcc1000 -w 2
-mysqldump --skip-extended-insert -uroot -pletmein -h127.0.0.1 tpcc1000 > all.sql
+./tpcc_load -h127.0.0.1 -uroot -pletmein -d tpcc1000 -w $1
+mysqldump --skip-extended-insert -uroot -pletmein -h127.0.0.1 tpcc1000 > all$1.sql
 mysql -uroot -pletmein -h127.0.0.1 -e "drop database if exists tpcc1000"
