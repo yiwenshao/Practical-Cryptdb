9e4de23c6db4cd035bd6d7bf72d34476b0618a29
casualet <shaoyiwenet@gmail.com>
2017-05-17 07:39:44 -0400

    compact mode

--- packages/tls/tpcc-mysql/cryptbenchmark.sh
+++ packages/tls/tpcc-mysql/cryptbenchmark.sh
@@ -3,6 +3,6 @@ mysql -uroot -pletmein -h127.0.0.1 -P3399 -e "create database tpcc1000"
 echo "=============================================CREATE TABLE====================================================="
 mysql -uroot -pletmein -h127.0.0.1 -P3399 tpcc1000 < create_table.sql
 echo "=============================================LOAD DATA====================================================="
-mysql -uroot -pletmein -h127.0.0.1 -P3399 tpcc1000 < all.sql
+mysql -uroot -pletmein -h127.0.0.1 -P3399 tpcc1000 < all$1.sql
 echo "=============================================DUMP DATABASE====================================================="
-mysqldump --skip-extended-insert -uroot -pletmein -h127.0.0.1 tpcc1000 > back.sql
+mysqldump --skip-extended-insert -uroot -pletmein -h127.0.0.1 --hex-blob tpcc1000 > back$1.sql
