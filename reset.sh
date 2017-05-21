dfb8ad277f3bee21bc49fc927222a20acfe07c2f
casualet <shaoyiwenet@gmail.com>
2017-05-21 17:03:46 +0800

    add reset

---
+++ reset.sh
@@ -0,0 +1,4 @@
+mysql -uroot -pletmein -h127.0.0.1 -e "drop database if exists tpcc1000"
+mysql -uroot -pletmein -h127.0.0.1 -e "drop database if exists tdb"
+mysql -uroot -pletmein -h127.0.0.1 -e "drop database if exists tdb2"
+rm -rf ./shadow/*
