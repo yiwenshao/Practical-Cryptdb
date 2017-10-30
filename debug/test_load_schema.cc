3d8f9cbe07c9a1aac578c70f8cf004547e8396a3
Casualet <shaoyiwenet@gmail.com>
2017-10-30 16:21:00 +0800

    able to use clean test_load_schema

--- debug/test_load_schema.cc
+++ debug/test_load_schema.cc
@@ -1,48 +1,15 @@
-#include <cstdlib>
-#include <cstdio>
-#include <string>
 #include <map>
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <stdexcept>
 #include <vector>
 #include <set>
-#include <list>
-#include <algorithm>
 #include <functional>
-#include <cctype>
-#include <locale>
 #include <unistd.h>
 #include <sys/types.h>
 #include <dirent.h>
-
 #include <main/Connect.hh>
+#include <main/Analysis.hh>
 #include <main/rewrite_main.hh>
 #include <main/rewrite_util.hh>
-#include <main/sql_handler.hh>
-#include <main/dml_handler.hh>
-#include <main/ddl_handler.hh>
-#include <main/metadata_tables.hh>
-#include <main/macro_util.hh>
-#include <main/CryptoHandlers.hh>
-
-#include <parser/embedmysql.hh>
-#include <parser/stringify.hh>
-#include <parser/lex_util.hh>
-
-#include <readline/readline.h>
-#include <readline/history.h>
-
-#include <crypto/ecjoin.hh>
-#include <util/errstream.hh>
-#include <util/cryptdb_log.hh>
-#include <util/enum_text.hh>
-#include <util/yield.hpp>
-
-#include <sstream>
-#include <unistd.h>
-
 
 static std::string embeddedDir="/t/cryt/shadow";
 
