491ba9355bc4a3378db116795a56f6f7f0bb5eeb
yiwenshao <shaoyiwenet@gmail.com>
2018-01-07 21:58:36 +0800

    modify reset.sh

--- reset.sh
+++ reset.sh
@@ -1,5 +1,5 @@
-mysql -uroot -pletmein -h127.0.0.1 -e "drop database if exists tpcc1000"
-mysql -uroot -pletmein -h127.0.0.1 -e "drop database if exists tdb"
-mysql -uroot -pletmein -h127.0.0.1 -e "drop database if exists tdb2"
-mysql -uroot -pletmein -h127.0.0.1 -e "drop database if exists tdb3"
+mysql -h127.0.0.1 -e "drop database if exists tpcc1000"
+mysql -h127.0.0.1 -e "drop database if exists tdb"
+mysql -h127.0.0.1 -e "drop database if exists tdb2"
+mysql -h127.0.0.1 -e "drop database if exists tdb3"
 rm -rf ./shadow/*
