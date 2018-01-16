e9dd8169e3e338ee86c107fe43c8184012af3883
yiwenshao <shaoyiwenet@gmail.com>
2018-01-16 20:53:38 +0800

    able to init micro bechmark

---
+++ debug/micro_init.cc
@@ -0,0 +1,28 @@
+#include "main/big_proxy.hh"
+#include <vector>
+using std::string;
+
+int
+main(int argc,char ** argv) {
+    if(argc!=2){
+        std::cout<<"expect 1 argument"<<std::endl;
+        return 0;
+    }
+    string length(argv[1]);
+
+    std::vector<string> create{
+        "drop database if exists micro_db;",
+        "create database micro_db;",
+        "use micro_db;",
+        "create table int_table(id integer);",
+        string("create table str_table(name varchar(")+length+"));"
+    };
+
+    big_proxy b;
+   
+    for(auto item:create){
+        b.go(item);
+    }
+    return 0;
+}
+
