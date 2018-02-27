731773f14135dee41c2741d862294dcdf32ca397
yiwenshao <shaoyiwenet@gmail.com>
2018-02-27 15:40:07 +0800

    add template for test_parser

--- test_parser/template.cc
+++ test_parser/template.cc
@@ -1,3 +1,29 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <main/Connect.hh>
+#include <main/rewrite_util.hh>
+#include <main/sql_handler.hh>
+#include <main/dml_handler.hh>
+#include <main/ddl_handler.hh>
+#include <main/CryptoHandlers.hh>
+#include "util/util.hh"
+static std::string embeddedDir="/t/cryt/shadow";
 int main() {
+    char *buffer;
+    if((buffer = getcwd(NULL, 0)) == NULL){
+        perror("getcwd error");
+    }
+    embeddedDir = std::string(buffer)+"/shadow";
+    init_mysql(embeddedDir);
+    std::string query = "insert into student values(NULL)";
+    std::unique_ptr<query_parse> p;
+    p = std::unique_ptr<query_parse>(
+                new query_parse("tdb", query));
+    UNUSED(p);
     return 0;
 }
