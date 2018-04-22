2e8810540b4c3ad4570197a9cf2444b88576641e
yiwenshao <shaoyiwenet@gmail.com>
2018-04-22 19:36:27 +0800

    add tools/test_global_insert

---
+++ tools/multi_fast_insert.cc
@@ -0,0 +1,54 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string>
+#include <vector>
+#include "redisbio/zmalloc.h"
+#include "redisbio/adlist.h"
+#include "redisbio/bio.h"
+
+//return 0 to continue, return 1 to exit.
+static
+int
+work(unsigned long id, void *input){
+    struct bio_job *task = (struct bio_job *)input;
+    if(task->stop == 1) return 1;
+    char * query = (char*)(task->arg1);
+    (void)query;
+    printf("id=%lu %s\n",id,query);
+    return 0;
+}
+
+int
+main(){
+    userFunction = work;
+    bioInit();
+    std::vector<std::string> input{
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j"
+    };
+    int type = 0;    
+    for(int i=0;i<10000;i++) {
+        bioCreateBackgroundJob(type,(input[i%10].c_str()),NULL,0);
+        type+=1;
+        type%=10;
+    }
+
+    //Only ten threads
+    for(int i=0;i<10;i++) {
+        bioCreateBackgroundJob(type,NULL,NULL,1);
+        type+=1;
+        type%=10;
+    }
+    printf("tobe joined\n");
+    bioKillThreads();
+    return 0;
+}
--- tools/test_global_insert.cc
+++ tools/test_global_insert.cc
@@ -1,3 +1,6 @@
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <iostream>
 #include <vector>
 #include <functional>
@@ -11,6 +14,10 @@
 #include <main/ddl_handler.hh>
 #include <main/CryptoHandlers.hh>
 #include <main/rewrite_main.hh>
+#include "util/util.hh"
+#include "redisbio/zmalloc.h"
+#include "redisbio/adlist.h"
+#include "redisbio/bio.h"
 
 extern CItemTypesDir itemTypes;
 static std::string embeddedDir="/t/cryt/shadow";
@@ -25,7 +32,9 @@ void myRewriteInsertHelper(const Item &i, const FieldMeta &fm, Analysis &a,
     }
 }
 
-static std::string getInsertResults(Analysis a,LEX* lex){
+
+static std::string 
+getInsertResults(Analysis a,LEX* lex){
         LEX *const new_lex = copyWithTHD(lex);
         const std::string &table =
             lex->select_lex.table_list.first->table_name;
@@ -33,21 +42,13 @@ static std::string getInsertResults(Analysis a,LEX* lex){
             lex->select_lex.table_list.first->db;
         //from databasemeta to tablemeta.
         const TableMeta &tm = a.getTableMeta(db_name, table);
-
         //rewrite table name
         new_lex->select_lex.table_list.first =
             rewrite_table_list(lex->select_lex.table_list.first, a);
-
         std::vector<FieldMeta *> fmVec;
-        std::vector<Item *> implicit_defaults;
-        
-        // No field list, use the table order.
-        assert(fmVec.empty());
         std::vector<FieldMeta *> fmetas = tm.orderedFieldMetas();
         fmVec.assign(fmetas.begin(), fmetas.end());
-
         if (lex->many_values.head()) {
-            //start processing many values
             auto it = List_iterator<List_item>(lex->many_values);
             List<List_item> newList;
             for (;;) {
@@ -57,27 +58,18 @@ static std::string getInsertResults(Analysis a,LEX* lex){
                 }
                 List<Item> *const newList0 = new List<Item>();
                 if (li->elements != fmVec.size()) {
-                    TEST_TextMessageError(0 == li->elements
-                                         && NULL == lex->field_list.head(),
-                                          "size mismatch between fields"
-                                          " and values!");
+                    exit(0);
                 } else {
                     auto it0 = List_iterator<Item>(*li);
                     auto fmVecIt = fmVec.begin();
-
                     for (;;) {
                         const Item *const i = it0++;
                         assert(!!i == (fmVec.end() != fmVecIt));
                         if (!i) {
                             break;
                         }
-                        //fetch values, and use fieldMeta to facilitate rewrite
-                        //every filed should be encrypted with onions of encryption
                         myRewriteInsertHelper(*i, **fmVecIt, a, newList0);
                         ++fmVecIt;
-                    }
-                    for (auto def_it : implicit_defaults) {
-                        newList0->push_back(def_it);
                     }
                 }
                 newList.push_back(newList0);
@@ -85,11 +77,37 @@ static std::string getInsertResults(Analysis a,LEX* lex){
             new_lex->many_values = newList;
         }
         return lexToQuery(*new_lex);
+        return "aa";
 }
 
-static void testInsertHandler(std::string query){
+SchemaInfo *gschema;
+AES_KEY *gkey;
+std::string gdb="tdb";
+
+static
+int
+ginsertFunction(unsigned long id,void *input){
+    const std::unique_ptr<AES_KEY> &TK = std::unique_ptr<AES_KEY>(gkey);
+    Analysis analysis(gdb,*gschema,TK,
+                        SECURITY_RATING::SENSITIVE);
+    std::unique_ptr<query_parse> p;
+    struct bio_job *task = (struct bio_job *)input;
+    if(task->stop == 1) return 1;
+    char * q = (char*)(task->arg1);
+    std::string query(q);
+    p = std::unique_ptr<query_parse>(
+                new query_parse(gdb, query));
+    LEX *const lex = p->lex();
+    std::cout<<getInsertResults(analysis,lex)<<std::endl;
+    (void)lex;
+    return 0;
+}
+
+static
+void
+testInsertHandler(){
     std::unique_ptr<Connect> e_conn(Connect::getEmbedded(embeddedDir));
-    std::unique_ptr<SchemaInfo> schema(new SchemaInfo());
+    gschema = new SchemaInfo();
     std::function<DBMeta *(DBMeta *const)> loadChildren =
         [&loadChildren, &e_conn](DBMeta *const parent) {
             auto kids = parent->fetchChildren(e_conn);
@@ -98,17 +116,8 @@ static void testInsertHandler(std::string query){
             }
             return parent;
         };
-    //load all metadata and then store it in schema
-    loadChildren(schema.get()); 
-    const std::unique_ptr<AES_KEY> &TK = std::unique_ptr<AES_KEY>(getKey(std::string("113341234")));
-    //just like what we do in Rewrite::rewrite,dispatchOnLex
-    Analysis analysis(std::string("tdb"),*schema,TK,
-                        SECURITY_RATING::SENSITIVE);
-    std::unique_ptr<query_parse> p;
-    p = std::unique_ptr<query_parse>(
-                new query_parse("tdb", query));
-    LEX *const lex = p->lex();
-    std::cout<<getInsertResults(analysis,lex)<<std::endl;
+    loadChildren(gschema); 
+    gkey = getKey(std::string("113341234"));
 }
 
 int
@@ -117,17 +126,28 @@ main() {
     if((buffer = getcwd(NULL, 0)) == NULL){
         perror("getcwd error");
     }
+    //Free to remove memory leak        
     embeddedDir = std::string(buffer)+"/shadow";
     const std::string master_key = "113341234";
     ConnectionInfo ci("localhost", "root", "letmein",3306);
+    UNUSED(ci);
+    //Clean!!
+    free(buffer);
+    //this function alone has memory leak
     SharedProxyState *shared_ps = new SharedProxyState(ci, embeddedDir , master_key, determineSecurityRating());
     assert(shared_ps!=NULL);
+    UNUSED(testInsertHandler);
+    UNUSED(getInsertResults);
     std::string query1 = "insert into student values(1,\"zhangfei\")";
-    std::vector<std::string> querys{query1};
-    for(auto item:querys){
-        std::cout<<item<<std::endl;
-        testInsertHandler(item);
-        std::cout<<std::endl;
+    std::vector<std::string> queries{query1};
+    for(unsigned int i=0u;i<100u;i++){
+        //queries.push_back(query1);
+    }
+    for(auto item:queries){
+        testInsertHandler();
+        struct bio_job *job = (struct bio_job*)zmalloc(sizeof(*job));
+        job->arg1 = (void*)(item.c_str());
+        ginsertFunction(1,job);
     }
     return 0;
 }
