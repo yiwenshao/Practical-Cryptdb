30cbd3f83fb8415b5c21f8769853e6a69d117a94
yiwenshao <shaoyiwenet@gmail.com>
2018-05-17 20:28:08 +0800

    add bench_pailliar

--- test_crypto/bench_pailliar.cc
+++ test_crypto/bench_pailliar.cc
@@ -29,18 +29,27 @@ test_pailliar(int num_of_tests) {
     ZZ pt0 = NTL::to_ZZ(1);
     ZZ enc0;
 
+    timer t;
     for(int i=0;i<num_of_tests;i++){
         enc0 = sk->encrypt(pt0);
     }
+    std::cout<<"enc_pailliar_in_us: "<<t.lap()*1.0/num_of_tests<<std::endl;
 
     for(int i=0;i<num_of_tests;i++){
         const ZZ dec0 = sk->decrypt(enc0);
     }
+    std::cout<<"dec_pailliar_in_us: "<<t.lap()*1.0/num_of_tests<<std::endl;
 }
-
 
 int
 main(int argc,char**argv) {
-    test_pailliar(100);
+    int num_of_tests = 10000;
+    if(argc==2){
+        num_of_tests = std::stoi(std::string(argv[1]));
+    }else{
+        std::cout<<"num_of_tests"<<std::endl;
+        return 0;
+    }        
+    test_pailliar(num_of_tests);
     return 0;
 }
